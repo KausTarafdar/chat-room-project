49a5a4cc1e033bda2628ba771ab43cb848c97a70
KausTarafdar <kaustarafdar@gmail.com>
2024-04-04 23:22:29 +0530

    deployment build scripts added

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,13 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./DB/connectionMongo.js";
 import { app, server } from "./socket/socket.js";
+import path from 'path'
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //to parse through the JSONS from req.body
 app.use(cookieParser()); //to parse through incoming cookies
@@ -19,6 +22,12 @@ app.use(cookieParser()); //to parse through incoming cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.get("/", (req, res) => {
   res.send("Hello World");
