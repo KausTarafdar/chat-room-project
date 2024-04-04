36d15447733e8a4ad45036769cd22bb3354ffa49
KausTarafdar <kaustarafdar@gmail.com>
2024-04-04 23:31:21 +0530

    updated socket url to live website

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProivder = ({children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://catapp-u3bq.onrender.com", {
                 query:{
                     userId: authUser._id
                 }
